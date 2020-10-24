cce493fd6bb275d554207106052148d5cbbf95f7
LarsFlieger <lars.flieger7@gmail.com>
2020-10-24 23:41:56 +0200

    init

---
+++ desktop/src/com/michael/blockbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.michael.blockbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.michael.blockbunny.BlockBunny;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlockBunny(), config);
+	}
+}
